9e05aaf7cc670b347f0bc8a3721e47fe9a78a799
Vitor Oikawa <vitor9998@gmail.com>
2021-05-24 21:14:00 -0300

    Criando Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    city = models.Charfield(max_length=100)
+    description = models.TextField()
+    phone = models.CharField(max_length=11)
+    email = models.EmailField()
+    begin_date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField()
+    active = models.BooleanField(default=True)
+
+
+
