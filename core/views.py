2cc7e954f44ebc353bece6078a7f2ede107679c4
Vitor Oikawa <vitor9998@gmail.com>
2021-10-02 22:46:48 -0300

    Continuação

--- core/views.py
+++ core/views.py
@@ -4,13 +4,14 @@ from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-
+from .models import Pet
 
 # Create your views here.
 
 @login_required(login_url='/login/')
 def list_all_pets(request):
-    return render(request, 'list.html')
+    pet = Pet.objects.filter(active=True)
+    return render(request, 'list.html', {'pet': pet})
 
 def logout_user(request):
     logout(request)
