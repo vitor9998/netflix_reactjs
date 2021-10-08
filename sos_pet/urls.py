20db4f8f3d0e5310e595097706a733a15d098252
Vitor Oikawa <vitor9998@gmail.com>
2021-10-07 22:39:01 -0300

    Continuação

--- sos_pet/urls.py
+++ sos_pet/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from core import views
 from django.views.generic import RedirectView
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from . import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,5 @@ urlpatterns = [
     path('logout/', views.logout_user),
     path('', RedirectView.as_view(url='pet/all/'))
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
